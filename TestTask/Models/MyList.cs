ef2a7e5585f6c1ebdd271e8a0b94f3a7dac029c4
Машимов Наурызбай <nauzik@mail.ru>
2024-03-14 10:44:42 +0500

    'AddProj'

---
+++ TestTask/Models/MyList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    public class MyList<T> : Collection<T>
+    {
+        //Get Count Items
+        public new int Count
+        {
+            get { return this.Items.Count; }
+        }
+
+        //Add 
+        public new void Add(T item)
+        {
+            //items.Add(item); 
+            base.Add(item);
+        }
+
+        //Get Value by index
+        public T GetValue(int item)
+        {
+            return this.ElementAt<T>(item);
+        }
+
+        //Remove Item by index
+        public new void RemoveItem(int index)
+        {
+            //items.RemoveAt(index);
+            this.RemoveItem(index);
+        }
+
+        //GetArray
+        //public void GetArray<T>()
+        //{
+
+        //}
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestTask.Models;
+
+MyList<int> list = [1, 2, 3, 4, 5];
+//1.Добавление нового значения
+list.Add(6);
+list.Add(7);
+
+foreach (var item in list)
+{
+    Console.WriteLine("Item = " + item.ToString());
+}
+
+//2.Получение значения 0 индекса
+Console.WriteLine("Value by 0 index = " + list.GetValue(0));
+
+//3.Получение кол-ва значении
+Console.WriteLine("Count = " + list.Count);
+
+//4.Удаление 1 индекса (с 0 начинается), получение кол-ва значении и перечисление всех
+Console.WriteLine("Delete value by index 1");
+list.RemoveItem(2);
+
+Console.WriteLine("Count = " + list.Count);
+foreach (var item in list)
+{
+    Console.WriteLine("Item = " + item.ToString());
+}
+
+//----------Дополнительно
+//1-Цикл foreach работает
+//2-LINQ запросы
+var select = (from s in list
+             where s == 2
+             select s).FirstOrDefault();
+Console.WriteLine("Select = " + select);
+Console.WriteLine("Where = " + list.Where(x => x == 2).FirstOrDefault());
+
+//3-расширяющий метод
+Console.ReadLine();
